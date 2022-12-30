853a93b0307c1de85b89795a445b22810eb3604a
George F <78384472+Wozunubi@users.noreply.github.com>
2022-12-29 20:11:14 -0500

    more efficient en/de functions

--- main.py
+++ main.py
@@ -5,56 +5,34 @@ TARGET_FOLDER = "./targetFolder"
 KEY_LOCATION = "./targetFolder/key"
 
 
-def encrypt(folder):
-    f = open(KEY_LOCATION, "at")
-    f.close()
-    f = open(KEY_LOCATION, "rt")
-    if f.read() == "":
-        f.close()
-        key = Fernet.generate_key()
-        fernet = Fernet(key)
-
-        for file in os.listdir(folder):
-            if os.path.isdir(os.path.join(folder, file)):
-                encrypt(os.path.join(folder, file))
-            elif file != "key":
-                f1 = open(folder + "/" + file, "rt")
-                f2 = open(folder + "/" + file + ".tmp", "wb")
-                f2.write(fernet.encrypt(f1.read().encode()))
-                f1 = open(folder + "/" + file, "wb")
-                f2 = open(folder + "/" + file + ".tmp", "rb")
-                f1.write(f2.read())
-                f2.close()
-                os.remove(folder + "/" + file + ".tmp")
-
-        f = open(KEY_LOCATION, "wb")
-        f.write(key)
-        f.close()
+def encrypt(fernet, folder):
+    for file in os.listdir(folder):
+        if os.path.isdir(os.path.join(folder, file)):
+            encrypt(fernet, os.path.join(folder, file))
+        elif file != "key":
+            f1 = open(folder + "/" + file, "rt")
+            f2 = open(folder + "/" + file + ".tmp", "wb")
+            f2.write(fernet.encrypt(f1.read().encode()))
+            f1 = open(folder + "/" + file, "wb")
+            f2 = open(folder + "/" + file + ".tmp", "rb")
+            f1.write(f2.read())
+            f2.close()
+            os.remove(folder + "/" + file + ".tmp")
 
 
-def decrypt(folder):
-    f = open(KEY_LOCATION, "rt")
-    if f.read() != "":
-        f.close()
-        f = open(KEY_LOCATION, "rb")
-        fernet = Fernet(f.read())
-        f.close()
-
-        for file in os.listdir(folder):
-            if os.path.isdir(os.path.join(folder, file)):
-                print("check")
-            elif file != "key":
-                f1 = open(folder + "/" + file, "rb")
-                f2 = open(folder + "/" + file + ".tmp", "wt")
-                f2.write(fernet.decrypt(f1.read()).decode())
-                f1 = open(folder + "/" + file, "wt")
-                f2 = open(folder + "/" + file + ".tmp", "rt")
-                f1.write(f2.read())
-                f2.close()
-                os.remove(folder + "/" + file + ".tmp")
-
-        f = open(KEY_LOCATION, "wb")
-        f.close()
+def decrypt(fernet, folder):
+    for file in os.listdir(folder):
+        if os.path.isdir(os.path.join(folder, file)):
+            print("check")
+        elif file != "key":
+            f1 = open(folder + "/" + file, "rb")
+            f2 = open(folder + "/" + file + ".tmp", "wt")
+            f2.write(fernet.decrypt(f1.read()).decode())
+            f1 = open(folder + "/" + file, "wt")
+            f2 = open(folder + "/" + file + ".tmp", "rt")
+            f1.write(f2.read())
+            f2.close()
+            os.remove(folder + "/" + file + ".tmp")
 
 
 if __name__ == '__main__':
@@ -62,6 +40,32 @@ if __name__ == '__main__':
     while action != "x":
         action = input()
         if action == "en":
-            encrypt(TARGET_FOLDER)
+            f = open(KEY_LOCATION, "at")
+            f.close()
+            f = open(KEY_LOCATION, "rt")
+
+            if f.read() == "":
+                f.close()
+                key = Fernet.generate_key()
+                fernet = Fernet(key)
+
+                encrypt(fernet, TARGET_FOLDER)
+
+                f = open(KEY_LOCATION, "wb")
+                f.write(key)
+
+            f.close()
         elif action == "de":
-            decrypt(TARGET_FOLDER)
+            f = open(KEY_LOCATION, "rt")
+
+            if f.read() != "":
+                f.close()
+                f = open(KEY_LOCATION, "rb")
+                fernet = Fernet(f.read())
+                f.close()
+
+                decrypt(fernet, TARGET_FOLDER)
+
+                f = open(KEY_LOCATION, "wb")
+
+            f.close()
